fc0a6183645850406cafa056621748cb2a67f893
przemus <przemyslaw.jan.pietrzak@gmail.com>
2016-08-11 20:20:47 +0200

    remove dead file

--- src/path.ts
+++ src/path.ts
@@ -1,19 +1,13 @@
 import createjs from "easel";
 
-import steps from "./map";
+import steps from "./mapPoint";
 
 const line: Shape = new createjs.Shape();
 line.graphics.beginStroke("black");
-
 line.graphics.moveTo(0, 0);
 
-let cacheX = 0;
-let cacheY = 0;
 steps.forEach(({ x, y }) => {
-  line.graphics.lineTo(cacheX + x, cacheY + y);
-  cacheX += x;
-  cacheY += y;
-
+  line.graphics.lineTo(x, y);
 });
 
 export default line;
