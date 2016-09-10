68e373d84a992d44951b174b8ae6076459d720b9
przemus <przemyslaw.jan.pietrzak@gmail.com>
2016-09-10 21:09:12 +0200

    productify

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-jspm bundle src/main.ts build/bundle.js
+jspm bundle-sfx src/main.ts build/bundle.js
+cp ./jspm_packages/github/kristopolous/BOOTSTRA.386@master/v2.3.1/bootstrap/css/bootstrap.css ./build
+cp ./jspm_packages/github/kristopolous/BOOTSTRA.386@master/v2.3.1/bootstrap/css/fonts/Fixedsys500c.woff ./build/fonts
