e69f2558c5f5fc85077c5dbb7fa42449a5a66b13
przemyslaw <przemyslaw.jan.pietrzak@gmail.com>
2016-12-05 17:19:29 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-jspm bundle-sfx src/main.ts build/bundle.js
+mkdir -p  build build/fonts
+cp -p index-build.html build/
+mv build/index-build.html build/index.html
+jspm build src/main.ts build/bundle.js --inject
 cp ./jspm_packages/github/kristopolous/BOOTSTRA.386@master/v2.3.1/bootstrap/css/bootstrap.css ./build
 cp ./jspm_packages/github/kristopolous/BOOTSTRA.386@master/v2.3.1/bootstrap/css/fonts/Fixedsys500c.woff ./build/fonts
