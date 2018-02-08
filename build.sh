beeca028a9a6ce05dfb2fd3cd61b1839f12a1de9
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2018-02-08 20:45:08 +0100

    clean up

--- build.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p  build build/fonts
-cp -p index-build.html build/
-mv build/index-build.html build/index.html
-jspm build src/main.ts build/bundle.js --inject
-cp ./jspm_packages/github/kristopolous/BOOTSTRA.386@master/v2.3.1/bootstrap/css/bootstrap.css ./build
-cp ./jspm_packages/github/kristopolous/BOOTSTRA.386@master/v2.3.1/bootstrap/css/fonts/Fixedsys500c.woff ./build/fonts
