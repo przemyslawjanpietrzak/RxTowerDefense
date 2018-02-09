3914d94a57494df566cc96f3b8301db1a1de12fb
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2018-02-09 14:26:39 +0100

    add unit tests for utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
-import Victor from "victor";
+import * as Victor from "victor";
 
-export const getDistance = (x1, y1, x2, y2) =>
+export const getDistance = (x1: number, y1: number, x2: number, y2: number): number =>
 	Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 
-export function getMove(
+export const getMove = (
 	position: {x: number, y: number},
 	destination: {x: number, y: number},
 	speed: number,
-): {x: number, y: number} {
+): {x: number, y: number} => {
 
 	const vector: Vector = new Victor(destination.x - position.x, destination.y - position.y);
 	const angle = vector.angle();
@@ -18,15 +18,15 @@ export function getMove(
 		x: newX,
 		y: newY,
 	};
-}
+};
 
-export function isInDistance(tower: Tower, destination: { x: number, y: number }): boolean {
+export const isInDistance = (tower: Tower, destination: { x: number, y: number }): boolean => {
 	return getDistance(tower.x, tower.y, destination.x, destination.y) <= tower.range;
-}
+};
 
-export function getTickerPerEnemy(counter: number, scenario): number {
+export const getTickerPerEnemy = (counter: number, scenario): number => {
 	const currentPart = String(Math.floor(counter / scenario.tickPerStep));
 	return scenario.parts[currentPart].tickPerEnemy;
-}
+};
 
 export const prop = (key: string) => (obj: object) => obj[key];
