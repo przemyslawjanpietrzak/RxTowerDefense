2d82b80479889a34d43b2ecafe10ab04f5370375
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2018-03-20 21:41:51 +0100

    upgrade linter

--- src/tower/sinks.ts
+++ src/tower/sinks.ts
@@ -1,4 +1,4 @@
-import { Observable, Subject } from 'rxjs/Rx';
+import { Subject } from 'rxjs/Rx';
 
 export const newTower$: Subject<object> = new Subject();
 export const towerFireToEnemy$: Subject<{tower: Tower, enemy: Enemy }> = new Subject();
