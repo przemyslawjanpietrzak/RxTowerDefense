9de496383b644a58a724c94fe5b855e7d00f6306
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2018-05-06 11:57:04 +0200

    prepare tower net class

---
+++ src/tower/net.ts
@@ -0,0 +1,28 @@
+import { FLOOR_SIZE } from '../scene/settings';
+
+import { Tower } from './models';
+import { StagePosition } from '../common/models';
+
+export class TowerNet {
+
+    private towers: Array<StagePosition> = [];
+
+    public addTower(position: StagePosition): void {
+        this.towers.push(
+            this.roundPosition(position)
+        );
+    }
+
+    public isSpaceForTower(position: StagePosition): boolean {
+        const { x, z } = this.roundPosition(position);
+
+        return !!this.towers.find(({ x: towerX, z: towerZ }) => towerX === x && towerZ === z);
+    }
+
+    private roundPosition({ x, z }: StagePosition): StagePosition {
+        return {
+            x: Math.round(x),
+            z: Math.round(z),
+        }
+    }
+}
